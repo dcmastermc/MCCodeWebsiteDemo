eb29978065e226a4627ad5ef45082df5f9e02041
dcmastermc <dcmastermc@gmail.com>
2017-07-16 20:19:18 +0200

    Added better nabber

---
+++ js/navbar.js
@@ -0,0 +1,46 @@
+var head = document.head || document.getElementsByTagName('head')[0];
+
+var secondaryNavbarStyle = document.getElementById("secondaryNavbarStyle");
+
+if (secondaryNavbarStyle == null) {
+    secondaryNavbarStyle = document.createElement("style");
+    secondaryNavbarStyle.type = "text/css";
+    secondaryNavbarStyle.id = "secondaryNavbarStyle";
+    head.appendChild(secondaryNavbarStyle);
+    secondaryNavbarStyle = document.getElementById("secondaryNavbarStyle");
+}
+
+function adjustSecondaryNavbar(scrollY) {
+    var height = 80 - scrollY;
+    if (height > 80) {
+        height = 80;
+    } else if (height < 60) {
+        height = 60;
+    }
+    var css = ".navbar-fixed-top { min-height: " + height + "; height: " + height + ";}.navbar-default .navbar-brand { line-height: " + height + ";}.navbar-default .navbar-nav>li>a { line-height: " + height + ";}html { height: calc(100% - " + height + ");}body { margin-top: " + height + ";}";
+    if (secondaryNavbarStyle.styleSheet) {
+        secondaryNavbarStyle.styleSheet.cssText = css;
+    } else {
+        secondaryNavbarStyle.innerHTML = css;
+    }
+}
+
+var documentElement = document.documentElement;
+var lastKnownScrollX = (window.pageXOffset || documentElement.scrollLeft) - (documentElement.clientLeft || 0);
+var lastKnownScrollY = (window.pageYOffset || documentElement.scrollTop) - (documentElement.clientTop || 0);
+
+var scrollTicking = false;
+
+window.addEventListener('scroll', function(e) {
+            var newScrollY = window.scrollY;
+            if (!scrollTicking) {
+                if (newScrollY != lastKnownScrollYPos) {
+                    window.requestAnimationFrame(function() {
+                        adjustSecondaryNavbar(newScrollY);
+                        scrollTicking = false;
+                    })
+                }
+            }
+            lastKnownScrollY = newScrollY;
+            scrollTicking = true;
+        }
