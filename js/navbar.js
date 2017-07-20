f765ca2ad0d28e2c5b15a3155bebd2ff5567e9f5
dcmastermc <dcmastermc@gmail.com>
2017-07-20 14:04:52 +0200

    Made javascript local

--- js/navbar.js
+++ js/navbar.js
@@ -1,75 +1,78 @@
-var head = document.head || document.getElementsByTagName("head")[0];
 
-var secondaryNavbarStyle = document.getElementById("secondaryNavbarStyle");
+var localNavbarJs = (function() {
+    var head = document.head || document.getElementsByTagName("head")[0];
 
-if (secondaryNavbarStyle == null) {
-    secondaryNavbarStyle = document.createElement("style");
-    secondaryNavbarStyle.type = "text/css";
-    secondaryNavbarStyle.id = "secondaryNavbarStyle";
-    head.appendChild(secondaryNavbarStyle);
-    secondaryNavbarStyle = document.getElementById("secondaryNavbarStyle");
-}
+    var secondaryNavbarStyle = document.getElementById("secondaryNavbarStyle");
 
-function adjustSecondaryNavbar(scrollY) {
-    var height = 80 - scrollY;
-    if (height > 80) {
-        height = 80;
-    } else if (height < 60) {
-        height = 60;
+    if (secondaryNavbarStyle == null) {
+        secondaryNavbarStyle = document.createElement("style");
+        secondaryNavbarStyle.type = "text/css";
+        secondaryNavbarStyle.id = "secondaryNavbarStyle";
+        head.appendChild(secondaryNavbarStyle);
+        secondaryNavbarStyle = document.getElementById("secondaryNavbarStyle");
     }
-    var css = ".navbar-fixed-top { min-height: " + height + "px; height: " + height + "px; } .navbar-default .navbar-brand { line-height: " + height + "px; } .navbar-default .navbar-brand>img { margin-top: " + (height / 8) + "px; height: " + (height / 4 * 3) + "px; } .navbar-default .navbar-nav>li>a { line-height: " + height + "px; } html { height: calc(100% - " + height + "px); } body { margin-top: " + height + "px; } ";
-    if (secondaryNavbarStyle.styleSheet) {
-        secondaryNavbarStyle.styleSheet.cssText = css;
-    } else {
-        secondaryNavbarStyle.innerHTML = css;
-    }
-}
 
-function unstyleSecondaryNavbar() {
-    if (secondaryNavbarStyle.styleSheet) {
-        secondaryNavbarStyle.styleSheet.cssText = "";
-    } else {
-        secondaryNavbarStyle.innerHTML = "";
-    }
-}
-
-var documentElement = document.documentElement;
-var lastKnownScrollX = (window.pageXOffset || documentElement.scrollLeft) - (documentElement.clientLeft || 0);
-var lastKnownScrollY = (window.pageYOffset || documentElement.scrollTop) - (documentElement.clientTop || 0);
-
-var scrollTicking = false;
-
-window.addEventListener("scroll", function(e) {
-    var deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-    if (deviceWidth >= 900) {
-        var newScrollY = window.scrollY;
-        if (!scrollTicking) {
-            window.requestAnimationFrame(function() {
-                adjustSecondaryNavbar(newScrollY);
-                scrollTicking = false;
-            });
+    function adjustSecondaryNavbar(scrollY) {
+        var height = 80 - scrollY;
+        if (height > 80) {
+            height = 80;
+        } else if (height < 60) {
+            height = 60;
         }
-        lastKnownScrollY = newScrollY;
-        scrollTicking = true;
-    }
-});
-
-if(window.attachEvent) {
-    window.attachEvent("onresize", function() {
-        var deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-        if (deviceWidth < 900) {
-            unstyleSecondaryNavbar();
+        var css = ".navbar-fixed-top { min-height: " + height + "px; height: " + height + "px; } .navbar-default .navbar-brand { line-height: " + height + "px; } .navbar-default .navbar-brand>img { margin-top: " + (height / 8) + "px; height: " + (height / 4 * 3) + "px; } .navbar-default .navbar-nav>li>a { line-height: " + height + "px; } html { height: calc(100% - " + height + "px); } body { margin-top: " + height + "px; } ";
+        if (secondaryNavbarStyle.styleSheet) {
+            secondaryNavbarStyle.styleSheet.cssText = css;
         } else {
-            adjustSecondaryNavbar(lastKnownScrollY);
+            secondaryNavbarStyle.innerHTML = css;
+        }
+    }
+
+    function unstyleSecondaryNavbar() {
+        if (secondaryNavbarStyle.styleSheet) {
+            secondaryNavbarStyle.styleSheet.cssText = "";
+        } else {
+            secondaryNavbarStyle.innerHTML = "";
+        }
+    }
+
+    var documentElement = document.documentElement;
+    var lastKnownScrollX = (window.pageXOffset || documentElement.scrollLeft) - (documentElement.clientLeft || 0);
+    var lastKnownScrollY = (window.pageYOffset || documentElement.scrollTop) - (documentElement.clientTop || 0);
+
+    var scrollTicking = false;
+
+    window.addEventListener("scroll", function(e) {
+        var deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        if (deviceWidth >= 900) {
+            var newScrollY = window.scrollY;
+            if (!scrollTicking) {
+                window.requestAnimationFrame(function() {
+                    adjustSecondaryNavbar(newScrollY);
+                    scrollTicking = false;
+                });
+            }
+            lastKnownScrollY = newScrollY;
+            scrollTicking = true;
         }
     });
-} else if(window.addEventListener) {
-    window.addEventListener("resize", function() {
-        var deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-        if (deviceWidth < 900) {
-            unstyleSecondaryNavbar();
-        } else {
-            adjustSecondaryNavbar(lastKnownScrollY);
-        }
-    }, true);
-}
+
+    if(window.attachEvent) {
+        window.attachEvent("onresize", function() {
+            var deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+            if (deviceWidth < 900) {
+                unstyleSecondaryNavbar();
+            } else {
+                adjustSecondaryNavbar(lastKnownScrollY);
+            }
+        });
+    } else if(window.addEventListener) {
+        window.addEventListener("resize", function() {
+            var deviceWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+            if (deviceWidth < 900) {
+                unstyleSecondaryNavbar();
+            } else {
+                adjustSecondaryNavbar(lastKnownScrollY);
+            }
+        }, true);
+    }
+}());
